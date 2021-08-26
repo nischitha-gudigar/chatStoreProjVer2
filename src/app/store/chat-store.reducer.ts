8fc24f964a924e9bb7f21428999c5faac208bed3
nischitha-gudigar <nischithagudigardr@gmail.com>
2021-08-26 12:56:59 +0530

    1 file modified

--- src/app/store/chat-store.reducer.ts
+++ src/app/store/chat-store.reducer.ts
@@ -6,11 +6,9 @@ const initialStateOnSave: chatActionState[] = [];
 export const chatSaveReducer = createReducer(
   initialStateOnSave,
   on(addChatOnSave, (state: chatActionState[], messageOnSave) => {
-    state = [...state];
-    state.concat(messageOnSave.messageData);
-    console.log(state);
     return {
-      ...state
+      ...state,
+      ...messageOnSave.messageData
     };
   })
 );
